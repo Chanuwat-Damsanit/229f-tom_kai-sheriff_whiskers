71e0a0b50b2040eed171009ba37016a3433ec7cb
Chanuwat-Damsanit <chanuwat.dams@bumail.net>
2025-05-04 14:24:34 +0700

    add GameScene and PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2d; //private variable
+
+    Vector2 moveInput; //for walk with addforce
+
+    //Walk left-right
+    float move; //store input from player
+    [SerializeField] float speed;
+
+    //Jump
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isJumping;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Walk with addforce
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.AddForce(moveInput * speed);
+
+        /*//Walk left-right
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);*/
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("Jump"); //for debugging
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+}
